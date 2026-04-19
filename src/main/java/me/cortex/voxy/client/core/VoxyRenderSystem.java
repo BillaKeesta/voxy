0c5d7defebfec1536ba311db3ec5886189314b15
mcrcortex <18544518+mcrcortex@users.noreply.github.com>
2026-04-19 17:48:20 +1000

    fix iris pain

--- src/main/java/me/cortex/voxy/client/core/VoxyRenderSystem.java
+++ src/main/java/me/cortex/voxy/client/core/VoxyRenderSystem.java
@@ -221,6 +221,10 @@ public class VoxyRenderSystem {
                 height = (int) (height*factor[1]);
             }
         }
+        if (width == 0 || height == 0) {
+            Logger.error("Viewport width or height was zero, this is bad bad bad");
+            return null;
+        }
 
         viewport
                 .setVanillaProjection(matrices.projection())
@@ -242,6 +246,7 @@ public class VoxyRenderSystem {
             return;
         }
         if (viewport.width <= 0 || viewport.height <= 0) {
+            Logger.error("Viewport width or height was zero, this is bad bad bad, exiting frame");
             return;//Only render on valid viewport
         }
 
